9ab81ca2de9a86aadc10cbfbcf39efb3e32acadb
Anya Kathpalia <92942248+anyakath@users.noreply.github.com>
2023-08-03 11:09:31 +0530

    Add files via upload

---
+++ src/components/MobileError.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Text2, Colors, MobileSpace, ErrorIcon } from '../ss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode} from '@fortawesome/free-solid-svg-icons';
+export default function MobileError() {
+  return (
+    <MobileSpace>
+        <br/> 
+        <ErrorIcon>
+          <FontAwesomeIcon icon={faCode} color = {Colors.grey2} size="8x" />
+          </ErrorIcon>
+        <Text2>
+            <br/>
+            Hi there! <br/> The mobile version of this website is unavailable at the moment. 
+            Please access the portfolio through your desktop.
+        </Text2>
+    </MobileSpace>
+
+  )
+}
